a9af3546e8d8312fa6a9519a89b9697551f138dd
Maycon Ribeiro <maycon_ribeiro@hotmail.com>
2021-02-02 20:51:29 -0300

    Methods GO

--- main/rectangle.go
+++ main/rectangle.go
@@ -1,14 +1,32 @@
 package main
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
-}
-
-func Area(width float64, height float64) float64 {
-	return width * height
-}
+import "math"
 
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+//func Area(rectangle Rectangle) float64 {
+//	return rectangle.Width * rectangle.Height
+//}
+
+//func Area(circle Circle) float64 {
+//	return circle.Radius
+//}
